2cd82edd90d6ad0d782d20089c7eb20a26614eb8
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2016-01-20 21:30:31 +0100

    MIPS: add Intel EASY330 evaluation board
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ include/configs/easy330.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016-2018 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ */
+
+#include <configs/intel_xway_common.h>
+#include <configs/intel_xway_grx330.h>
+
+#define CONFIG_ENV_SIZE			(8 * 1024)
